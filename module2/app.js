f26a2c5672abd786af96f46cad3b2da5948d72d2
Praveen Pagidi <praveenpagidi0@gmail.com>
2024-07-14 12:37:54 +0530

    setting sequelize to create table

--- module2/app.js
+++ module2/app.js
@@ -1,24 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-// const expressHandleBars = require("express-handlebars");
 
 const adminRoutes = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 const pageNotFoundController = require('./controllers/pageNotFound');
+const sequelize = require('./utils/database');
 
 const app = express();
 
-// app.engine(
-//   "hbs",
-//   expressHandleBars({
-//     layoutsDir: "views/layout/",
-//     defaultLayout: "main-layout",
-//     extname: 'hbs'
-//   })
-// ); // handlebars needs to be specifically mentioned seperately compared to pug as it does not comes built in express to handle it.
-
-// app.set() functions allows us to set global variables acroos our app, but certain variable are predifined such "view engine" to set template engine and "views" for the folder where our html files are.
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -30,4 +20,9 @@ app.use(shopRoute);
 
 app.use(pageNotFoundController.pageNotFound);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => console.log('sequelize error', error));
